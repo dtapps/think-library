d6d1e3251e95342610956a3f88ef72823600830c
Chaim <gc@dtapp.net>
2020-04-13 14:06:02 +0800

    test

---
+++ README.md
@@ -0,0 +1,32 @@
+<img align="right" width="100" src="https://cdn.oss.liguangchun.cn/04/999e9f2f06d396968eacc10ce9bc8a.png" alt="dtApp Logo"/>
+
+<h1 align="left"><a href="https://www.dtapp.net/">ThinkPhp扩展包</a></h1>
+
+📦 ThinkPhp扩展包
+
+[![Latest Stable Version](https://poser.pugx.org/liguangchun/think-library/v/stable)](https://packagist.org/packages/liguangchun/think-library) 
+[![Latest Unstable Version](https://poser.pugx.org/liguangchun/think-library/v/unstable)](https://packagist.org/packages/liguangchun/think-library) 
+[![Total Downloads](https://poser.pugx.org/liguangchun/think-library/downloads)](https://packagist.org/packages/liguangchun/think-library) 
+[![License](https://poser.pugx.org/liguangchun/think-library/license)](https://packagist.org/packages/liguangchun/think-library)
+
+## 依赖环境
+
+1. PHP7.0 版本及以上（低版本和7.4版本未做兼容处理！）
+
+## 安装
+
+```text
+composer require liguangchun/think-library ^6.x-dev -vvv
+```
+
+## 更新
+
+```text
+composer update liguangchun/think-library -vvv
+```
+
+## 删除
+
+```text
+composer remove liguangchun/think-library -vvv
+```
---
+++ src/Service.php
@@ -0,0 +1,63 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | ThinkLibrary 6.0 for ThinkPhP 6.0
+// +----------------------------------------------------------------------
+// | 版权所有 2017~2020 [ https://www.dtapp.net ]
+// +----------------------------------------------------------------------
+// | 官方网站: https://gitee.com/liguangchun/ThinkLibrary
+// +----------------------------------------------------------------------
+// | 开源协议 ( https://mit-license.org )
+// +----------------------------------------------------------------------
+// | gitee 仓库地址 ：https://gitee.com/liguangchun/ThinkLibrary
+// | github 仓库地址 ：https://github.com/GC0202/ThinkLibrary
+// | Packagist 地址 ：https://packagist.org/packages/liguangchun/think-library
+// +----------------------------------------------------------------------
+
+namespace DtApp\ThinkLibrary;
+
+use think\App;
+use think\Container;
+
+/**
+ * 自定义服务基类
+ * Class Service
+ * @package DtApp\ThinkLibrary
+ */
+abstract class Service
+{
+    /**
+     * 应用实例
+     * @var App
+     */
+    protected $app;
+
+    /**
+     * Service constructor.
+     * @param App $app
+     */
+    public function __construct(App $app)
+    {
+        $this->app = $app;
+        $this->initialize();
+    }
+
+    /**
+     * 初始化服务
+     * @return $this
+     */
+    protected function initialize()
+    {
+        return $this;
+    }
+
+    /**
+     * 静态实例对象
+     * @param array $args
+     * @return static
+     */
+    public static function instance(...$args)
+    {
+        return Container::getInstance()->make(static::class, $args);
+    }
+}
