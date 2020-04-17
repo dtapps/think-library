9ce6fa05b0f79df999b4d96bb1df85fa55c6b407
Chaim <gc@dtapp.net>
2020-04-17 16:39:57 +0800

    - update facade

---
+++ src/Random.php
@@ -0,0 +1,89 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | ThinkLibrary 6.0 for ThinkPhP 6.0
+// +----------------------------------------------------------------------
+// | 版权所有 2017~2020 [ https://www.dtapp.net ]
+// +----------------------------------------------------------------------
+// | 官方网站: https://gitee.com/liguangchun/ThinkLibrary
+// +----------------------------------------------------------------------
+// | 开源协议 ( https://mit-license.org )
+// +----------------------------------------------------------------------
+// | gitee 仓库地址 ：https://gitee.com/liguangchun/ThinkLibrary
+// | github 仓库地址 ：https://github.com/GC0202/ThinkLibrary
+// | Packagist 地址 ：https://packagist.org/packages/liguangchun/think-library
+// +----------------------------------------------------------------------
+
+namespace DtApp\ThinkLibrary;
+
+/**
+ * 随机管理类
+ * Class Ints
+ * @mixin Ints
+ * @package DtApp\ThinkLibrary
+ */
+class Random
+{
+    /**
+     * 生成随机
+     * @param int $length 长度
+     * @param int $type 类型，1 纯数字，2 纯小写字母，3 纯大写字母，4 数字和小写字母，5 数字和大写字母，6 大小写字母，7 数字和大小写字母
+     * @return false|string
+     */
+    public function generate(int $length = 6, int $type = 1)
+    {
+        // 取字符集数组
+        $number = range(0, 9);
+        $lowerLetter = range('a', 'z');
+        $upperLetter = range('A', 'Z');
+        // 根据type合并字符集
+        if ($type == 1) {
+            $charset = $number;
+        } elseif ($type == 2) {
+            $charset = $lowerLetter;
+        } elseif ($type == 3) {
+            $charset = $upperLetter;
+        } elseif ($type == 4) {
+            $charset = array_merge($number, $lowerLetter);
+        } elseif ($type == 5) {
+            $charset = array_merge($number, $upperLetter);
+        } elseif ($type == 6) {
+            $charset = array_merge($lowerLetter, $upperLetter);
+        } elseif ($type == 7) {
+            $charset = array_merge($number, $lowerLetter, $upperLetter);
+        } else {
+            $charset = $number;
+        }
+        $str = '';
+        // 生成字符串
+        for ($i = 0; $i < $length; $i++) {
+            $str .= $charset[mt_rand(0, count($charset) - 1)];
+            // 验证规则
+            if ($type == 4 && strlen($str) >= 2) {
+                if (!preg_match('/\d+/', $str) || !preg_match('/[a-z]+/', $str)) {
+                    $str = substr($str, 0, -1);
+                    $i = $i - 1;
+                }
+            }
+            if ($type == 5 && strlen($str) >= 2) {
+                if (!preg_match('/\d+/', $str) || !preg_match('/[A-Z]+/', $str)) {
+                    $str = substr($str, 0, -1);
+                    $i = $i - 1;
+                }
+            }
+            if ($type == 6 && strlen($str) >= 2) {
+                if (!preg_match('/[a-z]+/', $str) || !preg_match('/[A-Z]+/', $str)) {
+                    $str = substr($str, 0, -1);
+                    $i = $i - 1;
+                }
+            }
+            if ($type == 7 && strlen($str) >= 3) {
+                if (!preg_match('/\d+/', $str) || !preg_match('/[a-z]+/', $str) || !preg_match('/[A-Z]+/', $str)) {
+                    $str = substr($str, 0, -2);
+                    $i = $i - 2;
+                }
+            }
+        }
+        return $str;
+    }
+}
---
+++ src/Str.php
@@ -0,0 +1,158 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | ThinkLibrary 6.0 for ThinkPhP 6.0
+// +----------------------------------------------------------------------
+// | 版权所有 2017~2020 [ https://www.dtapp.net ]
+// +----------------------------------------------------------------------
+// | 官方网站: https://gitee.com/liguangchun/ThinkLibrary
+// +----------------------------------------------------------------------
+// | 开源协议 ( https://mit-license.org )
+// +----------------------------------------------------------------------
+// | gitee 仓库地址 ：https://gitee.com/liguangchun/ThinkLibrary
+// | github 仓库地址 ：https://github.com/GC0202/ThinkLibrary
+// | Packagist 地址 ：https://packagist.org/packages/liguangchun/think-library
+// +----------------------------------------------------------------------
+
+namespace DtApp\ThinkLibrary;
+
+/**
+ * 字符串管理类
+ * Class Ints
+ * @mixin Ints
+ * @package DtApp\ThinkLibrary
+ */
+class Str
+{
+    /**
+     * 截取字符串前面n个字符
+     * @param string $str 字符串
+     * @param int $start_num 开始位置
+     * @param int $end_num 多少个
+     * @return bool|false|string
+     */
+    public function extractBefore(string $str, int $start_num, int $end_num)
+    {
+        if (strlen($str) < $start_num + $end_num) return $str;
+        return substr($str, $start_num, $end_num);
+    }
+
+    /**
+     * 截取字符串最后n个字符
+     * @param string $str 字符串
+     * @param int $num 多少个
+     * @return false|string
+     */
+    public function extractRear(string $str, int $num)
+    {
+        if (strlen($str) <= $num) return $str;
+        return substr($str, -$num);
+    }
+
+    /**
+     * 过滤字符串
+     * @param string $str
+     * @return string
+     */
+    public function filter(string $str)
+    {
+        $str = str_replace('`', '', $str);
+        $str = str_replace('·', '', $str);
+        $str = str_replace('~', '', $str);
+        $str = str_replace('!', '', $str);
+        $str = str_replace('！', '', $str);
+        $str = str_replace('@', '', $str);
+        $str = str_replace('#', '', $str);
+        $str = str_replace('$', '', $str);
+        $str = str_replace('￥', '', $str);
+        $str = str_replace('%', '', $str);
+        $str = str_replace('^', '', $str);
+        $str = str_replace('……', '', $str);
+        $str = str_replace('&', '', $str);
+        $str = str_replace('*', '', $str);
+        $str = str_replace('(', '', $str);
+        $str = str_replace(')', '', $str);
+        $str = str_replace('（', '', $str);
+        $str = str_replace('）', '', $str);
+        $str = str_replace('-', '', $str);
+        $str = str_replace('_', '', $str);
+        $str = str_replace('——', '', $str);
+        $str = str_replace('+', '', $str);
+        $str = str_replace('=', '', $str);
+        $str = str_replace('|', '', $str);
+        $str = str_replace('\\', '', $str);
+        $str = str_replace('[', '', $str);
+        $str = str_replace(']', '', $str);
+        $str = str_replace('【', '', $str);
+        $str = str_replace('】', '', $str);
+        $str = str_replace('{', '', $str);
+        $str = str_replace('}', '', $str);
+        $str = str_replace(';', '', $str);
+        $str = str_replace('；', '', $str);
+        $str = str_replace(':', '', $str);
+        $str = str_replace('：', '', $str);
+        $str = str_replace('\'', '', $str);
+        $str = str_replace('"', '', $str);
+        $str = str_replace('“', '', $str);
+        $str = str_replace('”', '', $str);
+        $str = str_replace(',', '', $str);
+        $str = str_replace('，', '', $str);
+        $str = str_replace('<', '', $str);
+        $str = str_replace('>', '', $str);
+        $str = str_replace('《', '', $str);
+        $str = str_replace('》', '', $str);
+        $str = str_replace('.', '', $str);
+        $str = str_replace('。', '', $str);
+        $str = str_replace('/', '', $str);
+        $str = str_replace('、', '', $str);
+        $str = str_replace('?', '', $str);
+        $str = str_replace('？', '', $str);
+        $str = str_replace('╮', '', $str);
+        $str = str_replace('(', '', $str);
+        $str = str_replace(')', '', $str);
+        $str = str_replace('r', '', $str);
+        $str = str_replace('ぷ', '', $str);
+        $str = str_replace('〆', '', $str);
+        $str = str_replace('ゞ', '', $str);
+        $str = str_replace('ヤ', '', $str);
+        $str = str_replace('ゼ', '', $str);
+        $str = str_replace('ǎ', '', $str);
+        $str = str_replace('ǎ', '', $str);
+        $str = str_replace('〆', '', $str);
+        $str = str_replace('む', '', $str);
+        $str = str_replace('§', '', $str);
+        $str = str_replace('上门', '', $str);
+        return trim($str);
+    }
+
+    /**
+     * 判断字符串是否包含某个字符
+     * @param $str
+     * @param int $nee
+     * @param string $del
+     * @return bool
+     */
+    public function exitContain(string $str, $nee = 3, $del = ',')
+    {
+        if (strpos($str, $del) !== false) {
+            $var = explode($del, $str);
+            foreach ($var as $v) {
+                if ($v == $nee) return true;
+            }
+            return false;
+        } else {
+            if ($str == $nee) return true;
+            return false;
+        }
+    }
+
+    /**
+     * 统计字符串长度
+     * @param string $str 字符串
+     * @return int
+     */
+    public function len(string $str)
+    {
+        return strlen($str);
+    }
+}
