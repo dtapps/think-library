2abfb71bf35a685f98a2210a8b760611848d977d
Chaim <gc@dtapp.net>
2020-05-08 13:18:45 +0800

    - update common.php

--- src/common.php
+++ src/common.php
@@ -51,14 +51,14 @@ if (!function_exists('get_ip')) {
         $ip = '0.0.0.0';
         if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
             //为了兼容百度的CDN，所以转成数组
-            $arr = explode(',', $_SERVER('HTTP_X_FORWARDED_FOR'));
+            $arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
             $pos = array_search('unknown', $arr);
             if (false !== $pos) unset($arr[$pos]);
             $ip = trim($arr[0]);
         } elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
-            $ip = $_SERVER('HTTP_CLIENT_IP');
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
         } elseif (isset($_SERVER['REMOTE_ADDR'])) {
-            $ip = $_SERVER('REMOTE_ADDR');
+            $ip = $_SERVER['REMOTE_ADDR'];
         }
         return $ip;
     }
